5ea561aa29fbee3cd99d9e7bf4864097adb4bae7
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2024-10-22 21:54:50 +0800

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -17,13 +17,17 @@ class Character:
     def print_me(self):
         self.print_basics()
 
-    def print_plot(self):
-        print("Once upon a time, there was a princess...")
-
 class wizard(Character):
     def __init__(self):
         Character.__init__(self)
-        self.magic = 100
+        self.magic = 50
+
+    def setter(self,name):
+        self.name = name
+        self.attack = 10
+        self.defence = 10
+        self.health = 30
+        self.experience = 90
 
     def print_me(self):
         self.print_basics()
@@ -32,23 +36,133 @@ class wizard(Character):
 class knight(Character):
     def __init__(self):
         Character.__init__(self)
-        self.armour = random.randint(0,100)
+        self.armour = 50
 
     def setter(self,name):
         self.name = name
-        self.attack = random.randint(0,100)
-        self.defence = random.randint(0,100)
-        self.__health = random.randint(0,100)
-        self.experience = random.randint(0,100)
-
-    def health_getter(self):
-        return self.__health
+        self.attack = 30
+        self.defence = 50
+        self.health = 80
+        self.experience = 30
 
     def print_me(self):
         self.print_basics()
         print("Armour: ",self.armour)
 
-Chandler = knight()
-Chandler.setter("Chandler")
-Chandler.print_me()
-print("Health: ",Chandler.health_getter())
+class archer(Character):
+    def __init__(self):
+        Character.__init__(self)
+        self.arrow = 30
+
+    def setter(self,name):
+        self.name = name
+        self.attack = 40
+        self.defence = 20
+        self.health = 50
+        self.experience = 60
+
+    def print_me(self):
+        self.print_basics()
+        print("Arrow: ",self.arrow)
+
+user_name = input("Enter your name: ")
+user_character = input("Enter the character you want to play Wizard(w) / Knight(k) / archer (a): ")
+
+opponent_type = random.randint(1,3)
+if opponent_type == 1:
+    opponent = wizard()
+    opponent.setter("Gandolf")
+    print("Your opponent is: WIZARD")
+    opponent.print_me()
+elif opponent_type == 2:
+    opponent = knight()
+    opponent.setter("Arthur")
+    print("Your opponent is: KNIGHT")
+    opponent.print_me()
+else:
+    opponent = archer()
+    opponent.setter("Robinhood")
+    print("Your opponent is: ARCHER")
+    opponent.print_me()
+
+print('\n')
+
+if user_character == 'w':
+    user_character = wizard()
+    user_character.setter(user_name)
+    print("You are: WIZARD")
+    user_character.print_me()
+    print('\n')
+    if opponent_type == 1:
+        print("Tie Game")
+    elif opponent_type == 2:
+        print("Your attack is 10 damage")
+        print("Your magic can cause 50 damage")
+        print("Because your opponent has an armour, magic power times by 4")
+        print("Your attack is 210, your opponent health is 180")
+        print("You attacked, opponent dies")
+        print("You win!")
+    else:
+        print("Your attack is 10")
+        print("Your magic is 50")
+        print("Because your opponent doesn't have an armour, nothing changed")
+        print("Your attack is 60, your opponent health is 70")
+        print("You attacked, opponent health remains 10")
+        print("Opponent attack is 40 damage")
+        print("Opponent arrow can cause 30 damage")
+        print("Opponent attack is 70, your health is 40")
+        print("Opponent attacked, you die")
+        print("You lost...")
+elif user_character == 'k':
+    user_character = knight()
+    user_character.setter(user_name)
+    print("You are: KNIGHT")
+    user_character.print_me()
+    print('\n')
+    if opponent_type == 1:
+        print("Your attack is 30 damage")
+        print("Your armour can cause 50 damage")
+        print("Because your opponent has magic, your armour can't harm him")
+        print("Your attack is 30, your opponent health is 40")
+        print("You attacked, opponent health remains 10")
+        print("Opponent attack is 10 damage")
+        print("Your opponent magic can cause 50 damage")
+        print("Because you have an armour, your opponent magic power times by 4")
+        print("Opponent attack is 210, your health is 180")
+        print("Opponent attacked, you die")
+        print("You lost...")
+    elif opponent_type == 2:
+        print("Tie Game")
+    else:
+        print("Your attack is 30 damage")
+        print("Your armour can cause 50 damage")
+        print("Your attack is 80, your opponent health is 70")
+        print("You attacked, opponent dies")
+        print("You win!")
+elif user_character == 'a':
+    user_character = archer()
+    user_character.setter(user_name)
+    print("You are: ARCHER")
+    user_character.print_me()
+    print('\n')
+    if opponent_type == 1:
+        print("Your attack is 40")
+        print("Your arrow can cause 30 damage")
+        print("Your attack is 70, your opponent health is 40")
+        print("You attacked, opponent dies")
+        print("You win!")
+    elif opponent_type == 2:
+        print("Your attack is 40")
+        print("Your arrow can cause 30 damage")
+        print("Because your opponent has an armour")
+        print("Your attack is 70, your opponent health is 180")
+        print("Your attacked, opponent health remains 110")
+        print("Opponent attack is 30 damage")
+        print("Your opponent armour can cause 50 damage")
+        print("Opponent attack is 80, your health is 70")
+        print("Opponent attacked, you die")
+        print("You lost...")
+    else:
+        print("Tie Game")
+else:
+    print("Error, input not found.")
